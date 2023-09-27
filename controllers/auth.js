e3020a90842db9af5aabd7312422d1b319e94823
cesrra <c3z4r.rincon@gmail.com>
2023-09-27 15:02:26 -0500

    Created the User Model, and refactor the auth.js controller, and applied some validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,35 @@
 const { response } = require("express")
+const User = require('../models/User')
 
-const createUser = (req, res = response) => {
-    const { name, email, password } = req.body
+const createUser = async (req, res = response) => {
+    
+    try {        
+        const { email } = req.body
+        let user = await User.findOne({ email })
+        
+        if ( user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya ha sido usado en otra cuenta'
+            })
+        }
 
-    res.status(201).json({
-        ok: true,
-        mgs: 'create',
-        name,
-        email,
-        password,
-    })
+        user = new User( req.body )
+        await user.save()
+        
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor comuniquece con el Administrador'
+        })
+    }
 }
 
 const loginUser =(req, res = response) => {
