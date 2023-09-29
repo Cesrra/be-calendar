4e1dfcfeb04433638237a91c4c260c6e0ea7180a
cesrra <c3z4r.rincon@gmail.com>
2023-09-29 11:29:56 -0500

    Added the Event model, the moment librery to working with Date, maked the logic in to controller and putted teh checks in the routes

--- controllers/events.js
+++ controllers/events.js
@@ -1,33 +1,103 @@
 const { response } = require("express");
+const Event = require("../models/Event");
 
 
 
-const getEvents = ( req, res = response ) => {
-    return res.status(200).json({
+const getEvents = async ( req, res = response ) => {
+    const events = await Event.find()
+                              .populate('user', 'name')
+    res.status(200).json({
         ok: true,
-        msg: 'getEvents'
+        events
     })
 }
 
-const createEvent = ( req, res = response ) => {
-    return res.status(200).json({
-        ok: true,
-        msg: 'createEvent'
-    })
+const createEvent = async ( req, res = response ) => {
+    const event = new Event( req.body )
+    
+    try {
+        event.user = req.uid
+        const eventSaved = await event.save()
+        res.status(201).json({
+            ok: true,
+            event: eventSaved,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor comuniquece con el administrador'
+        })
+    }
 }
 
-const updateEvents = ( req, res = response ) => {
-    const id = req.getEvents
-    return res.status(200).json({
-        ok: true,
-        msg: 'updateEvents'
-    })
+const updateEvents = async ( req, res = response ) => {
+    const eventId = req.params.id
+    const uid = req.uid
+
+    try {
+        const event = await Event.findById({_id: eventId})
+
+        if ( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe evento con ese ID'
+            })
+        }
+
+        if ( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene permisos para modificar el evento'
+            })
+        }
+
+        const eventNew = {
+            ...req.body,
+            user: uid
+        }
+
+        const updatedEvent = await Event.findByIdAndUpdate( eventId, eventNew, { new: true} )
+
+        res.status(200).json({
+            ok: true,
+            event: updatedEvent
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false, 
+            msg: 'Por favor comuníquese con el adminstrador'
+        })
+    }
 }
 
-const deleteEvent = ( req, res = response ) => {
-    return res.status(200).json({
+const deleteEvent = async ( req, res = response ) => {
+    const eventId = req.params.id
+    const uid = req.uid
+
+    const event = await Event.findById({_id: eventId})
+    
+    if ( !event ) {
+        return res.status(404).json({
+            ok: false,
+            msg: 'No existe evento con ese ID'
+        })
+    }
+    
+    if ( event.user.toString() !== uid ) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'No tiene permisos para eliminar el evento'
+        })
+    }
+    
+    const eventDeleted = await Event.findByIdAndDelete({_id: eventId})
+
+    res.status(200).json({
         ok: true,
-        msg: 'deleteEvent'
+        event: eventDeleted
     })
 }
 
--- routes/events.js
+++ routes/events.js
@@ -7,14 +7,35 @@ const { Router } = require('express')
 const router = Router()
 const { getEvents, createEvent, updateEvents, deleteEvent } = require('../controllers/events')
 const { validateJWT } = require('../middlewares/validate-jwt')
+const { check } = require('express-validator')
+const { fieldValidators } = require('../middlewares/fields-validators')
+const { isDate } = require('../helpers/isDate')
 
 router.use( validateJWT )
 
 router.get( '/', getEvents )
 
-router.post( '/', createEvent )
+router.post(
+    '/',
+    [
+        check('title', 'El título es obligatorio').not().isEmpty(),
+        check('start', 'La fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'La fecha de finalización es obligatoria').custom( isDate ),
+        fieldValidators,
+    ],
+    createEvent
+)
 
-router.put( '/:id', updateEvents )
+router.put(
+    '/:id',
+    [
+        check('title', 'El título es obligatorio').not().isEmpty(),
+        check('start', 'La fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'La fecha de finalización es obligatoria').custom( isDate ),
+        fieldValidators,
+    ],
+    updateEvents
+)
 
 router.delete( '/:id', deleteEvent )
 
